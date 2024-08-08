6845f21ecfe3019d727ca50944c7ac2c84f8444c
Paul Scanlon <pauliescanlon@gmail.com>
2024-08-08 10:39:40 +0100

    chore: initial commit

---
+++ src/components/luzmo-client-component.tsx
@@ -0,0 +1,10 @@
+interface Props {
+  authKey: string;
+  authToken: string;
+}
+
+export default function LuzmoClientComponent({ authKey, authToken }: Props) {
+  console.log({ authKey, authToken });
+
+  return <section>Luzmo client component</section>;
+}
