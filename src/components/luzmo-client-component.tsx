45a898b58e1d843c3d78b0154e565157c01c0640
Paul Scanlon <pauliescanlon@gmail.com>
2024-11-03 09:38:07 +0700

    feat: add LuzmoAIChatComponent

--- src/components/luzmo-client-component.tsx
+++ src/components/luzmo-client-component.tsx
@@ -1,10 +1,23 @@
+import { LuzmoAIChatComponent } from '@luzmo/react-embed';
+
 interface Props {
   authKey: string;
   authToken: string;
 }
 
 export default function LuzmoClientComponent({ authKey, authToken }: Props) {
-  console.log({ authKey, authToken });
-
-  return <section>Luzmo client component</section>;
+  return (
+    <section>
+      <LuzmoAIChatComponent
+        appServer={import.meta.env.PUBLIC_LUZMO_APP_SERVER}
+        apiHost={import.meta.env.PUBLIC_LUZMO_API_HOST}
+        authKey={authKey}
+        authToken={authToken}
+        options={{
+          showChart: true,
+          showConfiguration: true,
+        }}
+      />
+    </section>
+  );
 }
